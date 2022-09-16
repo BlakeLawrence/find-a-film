3da0f62522b303f231f6a6aef7a16dd0514ccb3f
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-16 14:39:56 +0100

    finalized card colors for text

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,7 @@ const MovieCard = ({ movie }) => {
   return (
     <div
       className="flex-col justify-center 
-     items-center rounded-xl bg-[#797979] m-4 shadow-lg shadow-black hover:shadow-lg hover:shadow-[#1982c4]  hover:scale-110"
+     items-center rounded-xl bg-[#484848] m-4 shadow-lg shadow-black hover:shadow-lg hover:shadow-[#1982c4]  hover:scale-105"
     >
       <img
         className="rounded-t-xl w-full"
@@ -16,9 +16,9 @@ const MovieCard = ({ movie }) => {
         alt={movie.Title}
       />
       <div>
-        <div className="pl-1 ">
-          <h2 className="text-lg pb-2 ">{movie.Title}:</h2>
-          <p className="text-md">{movie.Year}</p>
+        <div className="pl-2 text-[#d4d4d4] ">
+          <h2 className="font-semibold text-md pt-1 ">{movie.Title}:</h2>
+          <p className="text-[#b1b2b2] text-sm pb-2 ">{movie.Year}</p>
         </div>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="flex justify-center items-center w-full h-[125px] bg-[#102542]  ">
-      <h1 className="text-5xl sm:text-7xl text-[#EDAE49] font-bold drop-shadow-xl shadow-[#EDAE49]">
+      <h1 className="font-rampart text-5xl sm:text-8xl text-[#EDAE49] drop-shadow-xl shadow-[#EDAE49]">
         Movie Mate
       </h1>
     </nav>
