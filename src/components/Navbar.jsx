b126db4694b5e9db0383f8af36b567a54c9a0def
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-21 09:30:25 +0100

    took away nav shadow and soprted logo margin and padding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,22 +4,22 @@ import logo from "../assets/camera.svg";
 const Navbar = () => {
   return (
     <nav
-      className="rotate-180 shadow-2xl shadow-[#EDAE49] flex justify-center items-center w-full h-[125px]
+      className="flex justify-center items-center w-full h-[125px]
      bg-[#102542]  "
     >
       <div className="flex items-center ">
         <img
           src={logo}
           alt="logo"
-          className="rotate-180  w-[25px] sm:w-[60px] mr-3 sm:mb-6"
+          className=" sm:mt-6 w-[25px] sm:w-[60px] mr-4 sm:mr-8 sm:mb-6"
         />
-        <h1 className="rotate-180 font-rampart text-[38px] sm:text-6xl md:text-7xl lg:text-8xl text-[#EDAE49]">
+        <h1 className=" font-rampart text-[38px] sm:text-6xl md:text-7xl lg:text-8xl text-[#EDAE49]">
           Movie Mate
         </h1>
         <img
           src={logo}
           alt="logo"
-          className="rotate-180 w-[25px] sm:w-[60px] ml-3 mt-1.5 sm:ml-6 sm:mb-6  "
+          className="sm:mt-8 w-[25px] sm:w-[60px] ml-4 mt-1.5 sm:ml-8 sm:mb-6  "
         />
       </div>
     </nav>
